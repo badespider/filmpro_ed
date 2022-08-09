fcf9afc9129c57256a0019029959c342c88276d2
badespider <80556496+badespider@users.noreply.github.com>
2022-08-08 21:59:09 -0400

    new file:   src/components/NavBar/styles.js

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AcountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+import useStyles from './styles';
 
 function NavBar() {
-  console.log('NavBar');
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
   return (
-    <div>NavBar</div>
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolBar}> {isMobile && (
+      <IconButton
+        color="inherit"
+        edge="start"
+        style={{ outline: 'none' }}
+        onClick={() => {}}
+        className={classes.menuButton}
+      ><Menu />
+      </IconButton>
+      )}
+      </Toolbar>
+    </AppBar>
   );
 }
 
